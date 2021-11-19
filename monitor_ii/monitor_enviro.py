ad0b5bd7ed5d16f66667d8a5c5fac16abb3f52e2
andrea-bradbury <79626254+andrea-bradbury@users.noreply.github.com>
2021-11-19 15:03:06 +0800

    feature/environment: Environmental data monitoring and storage

---
+++ monitor_ii/monitor_enviro.py
@@ -0,0 +1,14 @@
+from db2 import Database
+
+# import the ORM items
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# import the Model classes for CPU and Storage
+from db import CPU, Storage, Base, EnvironmentalTPH
+
+# import the methods that will be used from the mypi file
+from mypi import \
+    get_serial, get_mac, get_host_name, \
+    get_cpu_temp, get_gpu_temp, get_maximum_cpu_load
+
